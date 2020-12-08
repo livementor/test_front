c4781a243eaa54ee2d6d6f2bf21c041df4b49de5
Kevin Fabre <kevin.fabre@gemmici.com>
2020-12-08 21:23:23 +0100

    Set style for conversation card in ConversationList

--- components/chat/ConversationList.vue
+++ components/chat/ConversationList.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <span>Conversations ids</span>
-    <div v-for="(id, index) in conversationsIds" :key="index">
-      <NuxtLink :to="`/chat/conversations/${id}`" class="text-blue-livementor">
-        {{ id }}
+    <div v-for="(conversation, i) in conversations" :key="i">
+      <NuxtLink :to="`/chat/conversations/${conversation.id}`">
+        <div
+          class="hover:bg-gray-100 mx-2 px-4 py-4 rounded-2xl"
+          v-bind:class="{
+            'bg-green-100': conversation.id === $route.params.id,
+          }"
+        >
+          <span class="font-bold mb-2">
+            {{ conversation.title }}
+          </span>
+          <div>
+            <span
+              v-for="(participant, j) in conversation.participants"
+              :key="j"
+              class="text-gray-600 text-xs"
+            >
+              {{ participant }}
+            </span>
+          </div>
+        </div>
       </NuxtLink>
     </div>
   </div>
@@ -15,13 +32,13 @@ import { Getter } from 'vuex-class'
 
 @Component
 export default class ConversationList extends Vue {
-  @Getter('conversations/getConversations') getConversations:any
+  @Getter('conversations/getConversations') getConversations: any
 
-  get conversationsIds () {
+  get conversations() {
     if (!this.$fire.auth.currentUser) {
       return []
     }
-    return Object.keys(this.getConversations)
+    return this.getConversations
   }
 }
 </script>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex">
-    <div class="w-1/4">
-      <span class="font-bold text-24 mb-20">
-        Chat Layout
-      </span>
-      <ConversationList class="border" />
+    <div class="w-1/4 border-r border-solid border-gray">
+      <div class="h-16 flex flex-row items-center px-6">
+        <NuxtLink to="/chat/conversations/">
+          <span class="font-bold text-24">
+            Chat Layout
+          </span>
+        </NuxtLink>
+      </div>
+      <ConversationList />
     </div>
     <NuxtChild class="w-3/4" />
   </div>
