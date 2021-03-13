41148eae176a2885e832a7ceaea453e8f02c9527
Serge Mazille <serge.mazille@gmail.com>
2021-03-13 21:34:50 +0100

    Import ConversationList component

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="flex">
     <div class="w-1/4">
-      <span class="font-bold text-24 mb-20">
-        Chat Layout
-      </span>
       <ConversationList class="border" />
     </div>
+
     <NuxtChild class="w-3/4" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
+import ConversationList from '~/components/chat/ConversationList.vue'
 
-@Component
+@Component({ components: { ConversationList } })
 export default class Chat extends Vue {
   @Watch('$store.state.users.authUser', { immediate: true })
-  onChange (value: any) {
-    if (value !== null && value !== undefined && value !== '') {
+  onChange(currentUserId: any) {
+    if (currentUserId) {
       this.$store.dispatch('conversations/fetchConversationsForCurrentUser')
     }
   }
