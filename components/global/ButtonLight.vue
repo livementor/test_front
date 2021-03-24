950306649e565035bbe23bef49602c87d51a988c
Mohamed M <m-ma-dev@users.noreply.github.com>
2021-03-24 08:51:45 +0100

    Added light button.

---
+++ components/global/ButtonLight.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="p-3 rounded border-2 border-blue-800 text-blue-800 hover:text-white hover:bg-blue-700"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Button extends Vue {
+  @Prop({ default: 'button' }) text!: string
+}
+</script>
+
+<style scoped>
+
+</style>
