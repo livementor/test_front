7709d805916e27f1243e98535e333159e9ec1100
Kevin Salles <kevin.salles@eureka-technology.com>
2021-06-11 20:59:46 +0200

    feature: add localization

--- components/global/LMHeader.vue
+++ components/global/LMHeader.vue
@@ -3,30 +3,42 @@
     <span class="text-center font-bold text-white text-24 m-auto">
       {{ $t('header.company') }}
     </span>
+    <locale-changer />
     <Button v-if="showLogout" :text="$t('header.logout')" class="absolute right-0 mr-16" @click.native="logout" />
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
-
-@Component
-export default class HeaderComponent extends Vue {
-  currentRoute = this.$route.name
-
-  @Watch('$route', { immediate: true })
-  onPropertyChanged (value: any, _: any) {
-    this.currentRoute = value.name
-  }
-
-  get showLogout () {
-    return this.$route.name !== 'login' && this.$route.name !== 'index'
-  }
-
-  logout () {
-    this.$fire.auth.signOut().then(() => {
-      this.$router.replace('/login')
-    })
+<script>
+import LocaleChanger from './LocaleChanger.vue'
+export default {
+  components: {LocaleChanger},
+  data() {
+    return {
+      currentRoute: this.$route.name
+    }
+  },
+  computed: {
+    showLogout () {
+      return this.$route.name !== 'login' && this.$route.name !== 'index'
+    },
+    route() {
+      return this.$route
+    },
+  },
+  methods: {
+    logout () {
+      this.$fire.auth.signOut().then(() => {
+        this.$router.replace('/login')
+      })
+    },
+  },
+  watch: {
+    route :{
+      immediate: true,
+      handler(value) {
+        this.currentRoute = value.name
+      }
+    }
   }
 }
 </script>
