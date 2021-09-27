9942132e884d80fa16dd3ed7c4472c8ed93c0a7c
Boris Kubiak <kubiakboris@gmail.com>
2021-09-27 10:35:37 +0200

    Fix blocking vuex bugs

--- store/messages/actions.js
+++ store/messages/actions.js
@@ -1,7 +1,7 @@
 export default {
 
-  createMessage (_, payload) {
-    const ref = this.$fire.firestore.collection('conversations').doc(payload.conversationId).collection('messages').doc()
+  async createMessage (_, payload) {
+    const ref = await this.$fire.firestore.collection('conversations').doc(payload.conversationId).collection('messages').doc()
     payload.message.id = ref.id
     payload.message.createdAt = Date.now()
     ref.set(payload.message)
